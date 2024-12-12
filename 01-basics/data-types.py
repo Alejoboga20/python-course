715a1e899a5358d960e79c1837e514435fde6d35
alejoboga20 <alejoboga19@gmail.com>
2024-12-12 11:56:25 -0300

    basic numbers

---
+++ 01-basics/data-types.py
@@ -0,0 +1,18 @@
+# Numbers: We can perform arithmetic operations on numbers
+# Integers: Whole numbers
+print(2 + 3)
+print(type(2))
+
+# Floats: Numbers with decimal points
+print(2.0 + 3.0)
+print(type(2.0))
+print(type(2 + 3.0))  # The result is a float
+
+# Arithmetic operations
+print(2 + 3)  # Addition
+print(2 - 3)  # Subtraction
+print(2 * 3)  # Multiplication
+print(5 / 3)  # Division
+print(2 ** 3)  # Exponentiation
+print(4 // 3)  # Floor division
+print(5 % 3)  # Modulus
