34dc800dc917d516a3b317a184661ed38e6bac3a
alejoboga20 <alejoboga19@gmail.com>
2024-12-12 12:00:56 -0300

    math functions

--- 01-basics/data-types.py
+++ 01-basics/data-types.py
@@ -16,3 +16,8 @@ print(5 / 3)  # Division
 print(2 ** 3)  # Exponentiation
 print(4 // 3)  # Floor division
 print(5 % 3)  # Modulus
+
+# Math functions
+print(round(3.14159))  # Round to the nearest whole number
+print(round(3.14159, 2))  # Round to 2 decimal places
+print(abs(-3.14159))  # Absolute value
