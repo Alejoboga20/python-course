23a0152403227f80d1cc5a5b9b411adc00884553
Alejo Boga <alejoboga19@gmail.com>
2026-04-16 16:02:28 -0300

    add basic variables

---
+++ 01-basics/variables.py
@@ -0,0 +1,18 @@
+PI = 3.14 # Constants
+
+first_name = 'alejo' # Strings
+last_name = 'boga'
+
+full_name = first_name + ' ' + last_name
+
+print(f"first_name {first_name}")
+print(f"last_name {last_name}")
+print(f"full_name {full_name}")
+
+print(id(first_name)) # print memory address
+
+_private_name = 'test' # just convention, not really private
+
+print(f"_private_name {_private_name}")
+
+
