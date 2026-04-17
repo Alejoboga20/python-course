6e06e19324ca67331303eb3ce2d97aabe4d44753
Alejo Boga <alejoboga19@gmail.com>
2026-04-17 15:05:34 -0300

    conditionals

---
+++ 01-basics/conditionals.py
@@ -0,0 +1,13 @@
+is_new_dev = False
+interest_python = True
+knows_python = True
+
+if is_new_dev:
+    print('He/She needs to Learn Python')
+else:
+    print("Already experienced")    
+
+if is_new_dev:
+    print("Welcome")
+elif knows_python:
+    print("You're ready to learn fastapi")
