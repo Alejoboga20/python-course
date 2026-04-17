fa92a30049b87b8d4676899ee47ffaf99613fcba
Alejo Boga <alejoboga19@gmail.com>
2026-04-17 16:08:17 -0300

    add bruno requests

--- fastapi-fundamentals/main.py
+++ fastapi-fundamentals/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI(title="FastAPI Fundamentals")
 
+BLOG_POST = [
+    {"id": 1, "title": "Hello from FastAPI",
+        "description": "First App using FastAPI"},
+    {"id": 2, "title": "Hello from NestJS",
+        "description": "First App using NestJS"},
+    {"id": 3, "title": "Hello from Rails", "description": "First App using Rails"},
+]
+
+
 @app.get("/")
 def home():
-    return { 'message': "Hello World", 'ok': True }
+    return {'message': "Hello FastAPI", 'ok': True}
+
+
+@app.get("/posts")
+def list_posts():
+    return {"data": BLOG_POST}
